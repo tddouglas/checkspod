bd720d7ed4a602d902192b2c4e175a967f881a3e
Tyler Douglas <tdouglas@vinpic.com>
2024-07-15 22:14:18 -0400

    Initial finetuning code rebased to new refactoring

--- audio_to_text/diarization/finetune.py
+++ audio_to_text/diarization/finetune.py
@@ -1,16 +1,28 @@
+import os
+
 from huggingface_hub import notebook_login
 from datasets import load_dataset, DatasetDict
 
 
 # TODO: In progress
 def finetune():
-    # Not sure how this works??
-    notebook_login()  # Used to link notebook to hugging face hub which can be used as version control
-
     common_voice = DatasetDict()
 
-    common_voice["train"] = load_dataset("mozilla-foundation/common_voice_11_0", "hi", split="train+validation",
-                                         use_auth_token=True)
-    common_voice["test"] = load_dataset("mozilla-foundation/common_voice_11_0", "hi", split="test", use_auth_token=True)
+    common_voice["train"] = load_dataset("mozilla-foundation/common_voice_13_0", "hi", split="train+validation",
+                                         token=os.getenv('HUGGING_FACE_AUTH_TOKEN'), trust_remote_code=True)
+    common_voice["test"] = load_dataset("mozilla-foundation/common_voice_13_0", "hi", split="test")
 
-    print(common_voice)
+    common_voice = common_voice.remove_columns(
+        ["age", "client_id", "gender", "locale", "path", "segment", "down_votes", "up_votes"])
+
+    # Mozilla Common_voice doesn't have helpful accent labels. Only 'indian' and 'native'
+
+
+def print_dataset(dataset: DatasetDict):
+    for split, dataset in dataset.items():
+        print(f"Split: {split}")
+        for idx, entry in enumerate(dataset):
+            accent = entry['accent']
+            if accent != '':
+                print(f"Entry {idx}: {entry}")
+                print("\n")
